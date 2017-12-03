2e47085dab85d903e41f4e05fb1636e766a901d2
Austin Hunt <huntaj@g.cofc.edu>
2017-12-02 19:11:05 -0500

    updated python thread files

--- DiningPhilosophers/hw3/backendThreads.py
+++ DiningPhilosophers/hw3/backendThreads.py
@@ -1,2 +1,26 @@
 #need to translate hw3.c to python in this file
 
+from threading import *
+from .dpsim import *
+
+
+def main():
+
+    mainThread = Thread(target=mainThreadAction)
+
+    print("Error creating main thead!")
+    exit(1)
+
+    try:
+        mainThread.join()
+    except:
+        print("Error joining!")
+        exit(2)
+
+    return 0;
+
+
+
+
+
+
--- DiningPhilosophers/hw3/dpsim.py
+++ DiningPhilosophers/hw3/dpsim.py
@@ -1,1 +1,108 @@
-#Need to translate dpsim.c to python here
+    #Need to translate dpsim.c to python here
+
+from threading import *
+import copy
+import time
+
+
+
+#Define constants
+NUM_PHILOSOPHERS = 5
+NUM_CHOPSTICKS = 5
+
+#Define static vars
+
+#array of chopsticks
+chopsticks = [] #capacity of 5
+#array of mutexes
+mutexArray = [] #will have 5 locks
+philosopherThreads = [] #will have 5 threads
+philosopherObjects = [] #will have 5 objects
+
+
+def mainThreadAction():
+    for i in range(0,NUM_CHOPSTICKS):
+        chopsticks[i] = -1
+    print("Chopsticks initialized!")
+
+    for j in range(0,NUM_PHILOSOPHERS):
+
+        philosopherThreads[j] = Thread(target = philosopherAction, args = (j))
+
+        print("Error creating philosopher thread!")
+
+        philosopherThreads[j].start()
+
+    print("Thread created for each philosopher!")
+
+    while True:
+        chopsticksCopy = copy.copy(chopsticks)
+
+        if chopsticksCopy[0] == 0 and chopsticksCopy[1] == 0:
+            p0Eating = True
+        else:
+            p0Eating = False
+
+        if chopsticksCopy[1] == 1 and chopsticksCopy[2] == 1:
+            p1Eating = True
+        else:
+            p1Eating = False
+
+        if chopsticksCopy[2] == 2 and chopsticksCopy[3] == 2:
+            p2Eating = True
+        else:
+            p2Eating= False
+
+        if chopsticksCopy[3] == 3 and chopsticksCopy[4] == 3:
+            p3Eating = True
+        else:
+            p3Eating = False
+
+        if chopsticksCopy[4] == 4 and chopsticksCopy[0] == 4:
+            p4Eating = True
+        else:
+            p4Eating = False
+
+        #WHich ones are eating?
+
+
+        print("\nPhilosopher(s) ")
+        if p0Eating:
+            print("0")
+        if p1Eating:
+            print("1")
+        if p2Eating:
+            print("")
+        if p3Eating:
+            print("3")
+        if p4Eating:
+            print("4")
+        print(" are eating.\n")
+
+    #Deadlock
+        if chopsticksCopy[0] == 0 and chopsticksCopy[1] == 1 and chopsticksCopy[2] == 2 \
+                and chopsticksCopy[3] == 3 and chopsticksCopy[4] == 4:
+            print("Deadlock condition (0,1,2,3,4) ... terminating.")
+            break
+
+        time.sleep(1)
+
+    for k in range(0,NUM_PHILOSOPHERS):
+        philosopherThreads[k].stop()
+
+    return
+
+#philosopher thread
+def philosopherAction(args):
+    philId = args
+    while True:
+        time.sleep(2)
+        eat(philId)
+
+def eat(phil_id):
+    with mutexArray[phil_id]:
+        chopsticks[phil_id] = phil_id
+
+        with mutexArray[(phil_id+1)%NUM_CHOPSTICKS]:
+            chopsticks[(phil_id + 1) % NUM_CHOPSTICKS] = phil_id
+            time.sleep(3) #eating 3 seconds
