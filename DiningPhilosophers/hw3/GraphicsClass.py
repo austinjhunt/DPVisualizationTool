8e26b33bb97e8bc181b236f7cd397b44622b4168
Austin Hunt <huntaj@g.cofc.edu>
2017-12-02 23:36:31 -0500

    update connection bw front end, back end

--- DiningPhilosophers/hw3/GraphicsClass.py
+++ DiningPhilosophers/hw3/GraphicsClass.py
@@ -1,87 +1,93 @@
 from graphics import *
 from math import *
 
-#class to create window, background (table) 
-class Table:
-    table1 = Circle(Point(250,250),130)
-    table1.setFill(color_rgb(0,0,0))
-    table2 = Circle(Point(250,250),100)
-    table2.setFill(color_rgb(50,50,50))
 
-class LChopStick:
+# class to create window, background (table)
+class Table:
+    table1 = Circle(Point(250, 250), 130)
+    table1.setFill(color_rgb(0, 0, 0))
+    table2 = Circle(Point(250, 250), 100)
+    table2.setFill(color_rgb(50, 50, 50))
+
+
+class ChopStick:
     datStick = None
-    def __init__(self, width, r, g, b):
-        self.datStick = Line(Point(30,60), Point(60,100))
+
+    def __init__(self, width, r, g, b, p1, p2,win):
+        self.datStick = Line(p1, p2)
         self.datStick.setWidth(width)
         self.datStick.setFill(color_rgb(r, g, b))
+        self.datStick.draw(win)
+
+
 class PhilosopherBoi:
+    myHead = Circle(Point(0,0),0)
+    RightArm = Line(Point(0,0),Point(0,0))
+    LeftArm = Line(Point(0,0),Point(0,0))
 
-    myHead = None
-
-
-
-    def __init__(self, r, g, b, x, y):
+    def __init__(self, r, g, b, x, y, win):
         self.myHead = Circle(Point(x, y), 50)
         self.myHead.setFill(color_rgb(r, g, b))
         self.myHead.setOutline(color_rgb(r, g, b))
+        self.myHead.draw(win)
 
-    # def pickUpRight(philId):
-    #     if philId = 0:
-    #         #create arm in necesssary position for THIS philosopher
-    #         #draw that arm
-    #
-    #     if philId = 1:
-    #         ##
-    #     if philId = 2:
-    #
-    #     if philId = 3:
-    #
-    #     if philId = 4:
+    def pickUpRight(self,philId, win):
+        if philId == 0:
+            RightArm = Line(Point(270, 398), Point(314, 321))
+            RightArm.setFill(color_rgb(255, 255, 255))
+            RightArm.setWidth(7)
+            RightArm.draw(win)
+        if philId == 1:
+            RightArm = Line(Point(113, 304), Point(205, 327))
+            RightArm.setFill(color_rgb(255, 255, 255))
+            RightArm.setWidth(7)
+            RightArm.draw(win)
+        if philId == 2:
+            RightArm = Line(Point(173, 130), Point(170, 219))
+            RightArm.setFill(color_rgb(255, 255, 255))
+            RightArm.setWidth(7)
+            RightArm.draw(win)
+        if philId == 3:
+            RightArm = Line(Point(344, 139), Point(253, 160))
+            RightArm.setWidth(7)
+            RightArm.setFill(color_rgb(255, 255, 255))
+            RightArm.draw(win)
+        if philId == 4:
+            RightArm = Line(Point(390, 278), Point(335, 215))
+            RightArm.setWidth(7)
+            RightArm.setFill(color_rgb(255,255,255))
+            RightArm.draw(win)
 
-    #def pickUpRight(PhilosopherId):
-     #   return 1
-
-#    def pickUpLeft(PhilosopherId):
- #       return 2
-        
-    
-
-def main():
-    degreeInterval = 72
-    point1x = cos(degreeInterval)
-    point1y = sin(degreeInterval)
-    point2x = cos(degreeInterval*2)
-    point2y = sin(degreeInterval*2)
-    point3x = cos(degreeInterval*3)
-    point3y = sin(degreeInterval*3)
-    point4x = cos(degreeInterval*4)
-    point4y = sin(degreeInterval*4)
-    point5x = cos(degreeInterval*5)
-    point5y = sin(degreeInterval*5)
-
+    def putDownLeft(self):
+        self.LeftArm.undraw()
+    def putDownRight(self):
+        self.RightArm.undraw()
 
 
-    win = GraphWin('Dining Philosophers', 500, 500)
-    table = Table()
-    table.table1.draw(win)
-    table.table2.draw(win)
-    philosoph = PhilosopherBoi(0,255,0,250 -(point1x * 200), 250 -(point1y * 200))
-    philosoph.myHead.draw(win)
-    philosoph2 = PhilosopherBoi(0, 255, 0, 250 - (point2x * 200), 250 - (point2y * 200))
-    philosoph2.myHead.draw(win)
-    philosoph3 = PhilosopherBoi(0, 255, 0, 250 - (point3x * 200), 250 - (point3y * 200))
-    philosoph3.myHead.draw(win)
-    leftChop1 = LChopStick(20,255, 255, 0)
-    leftChop1.datStick.draw(win)
-    #create philosopher
-    #boi1 = PhilosopherBoi(255, 255, 0, point1x * 100, point1y * 100)
-    #philosopherCircle = boi1.makePhilosopher()
+    def pickUpLeft(self,philId, win):
+        if philId == 0:
+            LeftArm = Line(Point(270, 398), Point(205, 327))
+            LeftArm.setFill(color_rgb(255, 255, 255))
+            LeftArm.setWidth(7)
+            LeftArm.draw(win)
+        if philId == 1:
+            LeftArm = Line(Point(113, 304), Point(170, 219))
+            LeftArm.setFill(color_rgb(255, 255, 255))
+            LeftArm.setWidth(7)
+            LeftArm.draw(win)
+        if philId == 2:
+            LeftArm = Line(Point(173, 130), Point(253,160))
+            LeftArm.setFill(color_rgb(255, 255, 255))
+            LeftArm.setWidth(7)
+            LeftArm.draw(win)
+        if philId == 3:
+            LeftArm = Line(Point(344, 139), Point(335,215))
+            LeftArm.setWidth(7)
+            LeftArm.setFill(color_rgb(255, 255, 255))
+            LeftArm.draw(win)
+        if philId == 4:
+            LeftArm = Line(Point(390, 278), Point(314, 321))
+            LeftArm.setWidth(7)
+            LeftArm.setFill(color_rgb(255,255,255))
+            LeftArm.draw(win)
 
-    #philosopherCircle.draw(win)
-
-    win.getMouse()
-    win.close()
-
-    
-
-main()
--- DiningPhilosophers/hw3/dpsim.py
+++ DiningPhilosophers/hw3/dpsim.py
@@ -1,8 +1,36 @@
 # Need to translate dpsim.c to python here
 
-from threading import *
 import copy
-import time
+from time import sleep
+from DiningPhilosophers.hw3.GraphicsClass import *
+from DiningPhilosophers.hw3.StoppableThread import *
+
+win = GraphWin('Dining Philosophers', 500, 500)
+
+# draw table and philosophers and chopsticks, ready to eat
+table = Table()
+table.table1.draw(win)
+table.table2.draw(win)
+
+philosoph0 = PhilosopherBoi(0, 220, 0, 270, 398, win)
+philosoph1 = PhilosopherBoi(0, 220, 0, 113, 304, win)
+philosoph2 = PhilosopherBoi(0, 220, 0, 173, 130, win)
+philosoph3 = PhilosopherBoi(0, 220, 0, 344, 139, win)
+philosoph4 = PhilosopherBoi(0, 220, 0, 390, 278, win)
+
+# Chopsticks
+chopstick0 = ChopStick(10, 255, 255, 0, Point(205, 327), Point(228, 295), win)
+
+chopstick1 = ChopStick(10, 255, 255, 0, Point(170, 219), Point(200, 229), win)
+
+chopstick2 = ChopStick(10, 255, 255, 0, Point(251, 160), Point(253, 185), win)
+
+chopstick3 = ChopStick(10, 255, 255, 0, Point(335, 215), Point(304, 223), win)
+
+chopstick4 = ChopStick(10, 255, 255, 0, Point(314, 321), Point(290, 295), win)
+
+# wait for click to start
+win.getMouse()
 
 # Define constants
 NUM_PHILOSOPHERS = 5
@@ -16,6 +44,11 @@ chopsticks = []  # capacity of 5
 mutexArray = []  # will have 5 locks
 philosopherThreads = []  # will have 5 threads
 philosopherObjects = []  # will have 5 objects
+philosopherObjects.append(philosoph0)
+philosopherObjects.append(philosoph1)
+philosopherObjects.append(philosoph2)
+philosopherObjects.append(philosoph3)
+philosopherObjects.append(philosoph4)
 
 
 def mainThreadAction():
@@ -24,65 +57,104 @@ def mainThreadAction():
     print("Chopsticks initialized!")
 
     for j in range(0, NUM_PHILOSOPHERS):
-        currPhilosopherThread = Thread(target=philosopherAction, args=[j])
+        id = "phil"+j.__str__()
+        currPhilosopherThread = StoppableThread(name=id,target=philosopherAction, args=[j])
         philosopherThreads.append(currPhilosopherThread)
 
         currPhilosopherThread.start()
 
     print("Thread created for each philosopher!")
 
-    #initialize array of mutex locks
+    # initialize array of mutex locks
 
     for m in range(0, NUM_PHILOSOPHERS):
-
         mutexArray.append(Lock())
-        print("APPENDING LOCK")
-    p0Eating=False
-    p1Eating=False
-    p2Eating=False
-    p3Eating=False
-    p4Eating=False
+
+    p0Eating = False
+    p1Eating = False
+    p2Eating = False
+    p3Eating = False
+    p4Eating = False
     while True:
+        philosoph0.LeftArm.undraw()
+        philosoph0.RightArm.undraw()
+
+        philosoph1.LeftArm.undraw()
+        philosoph1.RightArm.undraw()
+
+        philosoph2.LeftArm.undraw()
+        philosoph2.RightArm.undraw()
+
+        philosoph3.LeftArm.undraw()
+        philosoph3.RightArm.undraw()
+
+        philosoph4.LeftArm.undraw()
+        philosoph4.RightArm.undraw()
+
+
         chopsticksCopy = copy.copy(chopsticks)
 
-        if chopsticksCopy[0] == 0 and chopsticksCopy[1] == 0:
-            p0Eating = True
+        if chopsticksCopy[0] == 0:
+            philosoph0.pickUpRight(0, win)
+            if chopsticksCopy[1] == 0:
+                philosoph0.pickUpLeft(0, win)
+                p0Eating = True
+            else:
+                p0Eating = False
         else:
             p0Eating = False
 
-        if chopsticksCopy[1] == 1 and chopsticksCopy[2] == 1:
-            p1Eating = True
+        if chopsticksCopy[1] == 1:
+            philosoph1.pickUpRight(1, win)
+            if chopsticksCopy[2] == 1:
+                philosoph1.pickUpLeft(1, win)
+                p1Eating = True
+            else:
+                p1Eating = False
         else:
             p1Eating = False
 
-        if chopsticksCopy[2] == 2 and chopsticksCopy[3] == 2:
-            p2Eating = True
+        if chopsticksCopy[2] == 2:
+            philosoph2.pickUpRight(2, win)
+            if chopsticksCopy[3] == 2:
+                philosoph2.pickUpLeft(2, win)
+                p2Eating = True
+            else:
+                p2Eating = False
         else:
             p2Eating = False
 
-        if chopsticksCopy[3] == 3 and chopsticksCopy[4] == 3:
-            p3Eating = True
+        if chopsticksCopy[3] == 3:
+            philosoph3.pickUpRight(3, win)
+            if chopsticksCopy[4] == 3:
+                philosoph3.pickUpLeft(3, win)
+                p3Eating = True
+            else:
+                p3Eating = False
         else:
             p3Eating = False
 
-        if chopsticksCopy[4] == 4 and chopsticksCopy[0] == 4:
-            p4Eating = True
+        if chopsticksCopy[4] == 4:
+            philosoph4.pickUpRight(4, win)
+            if chopsticksCopy[0] == 4:
+                philosoph4.pickUpLeft(4, win)
+                p4Eating = True
         else:
             p4Eating = False
 
         # WHich ones are eating?
 
-        print("\nPhilosopher(s) ")
+        print("\nPhilosopher(s) ",end="")
         if p0Eating:
-            print("0")
+            print("0",end=" ")
         if p1Eating:
-            print("1")
+            print("1",end=" ")
         if p2Eating:
-            print("")
+            print("", end= " ")
         if p3Eating:
-            print("3")
+            print("3",end= " ")
         if p4Eating:
-            print("4")
+            print("4",end= " ")
         print(" are eating.\n")
 
         # Deadlock
@@ -91,37 +163,43 @@ def mainThreadAction():
             print("Deadlock condition (0,1,2,3,4) ... terminating.")
             break
 
-        time.sleep(.1)
+        time.sleep(.5)
+        # Remove all arms for next iteration
+        print("Time to undraw!")
+        philosoph0.putDownLeft()
+        philosoph0.putDownRight()
+
+        philosoph1.putDownLeft()
+        philosoph1.putDownRight()
+
+        philosoph2.putDownLeft()
+        philosoph2.putDownRight()
+
+        philosoph3.putDownLeft()
+        philosoph3.putDownRight()
+
+        philosoph4.putDownLeft()
+        philosoph4.putDownRight()
 
     for k in range(0, NUM_PHILOSOPHERS):
-        philosopherThreads[k].stop()
-
-    return
+        philosopherThreads[k].stop_me()
 
 
 # philosopher thread
 def philosopherAction(philId):
     while True:
-        time.sleep(.01)
+        time.sleep(.2)
         eat(philId)
 
 
 def eat(phil_id):
-
     with mutexArray[phil_id]:
         chopsticks[phil_id] = phil_id
 
         with mutexArray[(phil_id + 1) % NUM_CHOPSTICKS]:
             chopsticks[(phil_id + 1) % NUM_CHOPSTICKS] = phil_id
-            time.sleep(4)  # eating 3 seconds
+
+            time.sleep(.3)  # eating 3 seconds
+        time.sleep(.5)
 
 
-
-def main():
-
-    mainThread = Thread(target=mainThreadAction())
-
-    mainThread.join()
-
-
-main()
